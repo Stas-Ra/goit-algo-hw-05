9536bbec7e687f712b6b5df831f66d12a332f00b
Stanislav Rakytianskyi <mail.sovik@gmail.com>
2024-11-07 21:48:02 +0200

    update_2

--- chw_5_task_2.py
+++ chw_5_task_2.py
@@ -2,13 +2,13 @@
 from typing import Callable
 import re
 
-
+  
 def generator_numbers(text):
-    pattern = r"\d+\.\d+"
+    pattern = r" \d+\.\d+\ "
     for value in re.findall(pattern, text):
         yield value
 
-def sum_profit(text, func: Callable):
+def sum_profit(text, funk):
     list_profit = []
     for value in generator_numbers(text):
         list_profit.append(float(value))
@@ -16,8 +16,8 @@ def sum_profit(text, func: Callable):
     return result
 
 text_profit = ("Загальний дохід працівника складається з декількох частин:"
-        " 1000.01 як основний дохід, доповнений додатковими"
-        " надходженнями 27.45 і 324.00 доларів.")
+        "1. 1000.01 як основний дохід, доповнений додатковими"
+        " надходженнями 2. 27.45 і 3. 324.00 доларів.")
 
 total_income = sum_profit(text_profit, generator_numbers)
 print(f"Загальний дохід: {total_income}")
